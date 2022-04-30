0cb861391c8325d8fc83a07fdedb72431a96b5c3
Maelkum <pathlidge.ann+github@gmail.com>
2022-04-30 22:55:08 +0200

    Add a specific type for collection trait map + remove obsolete code

--- graph/api/traits.go
+++ graph/api/traits.go
@@ -4,44 +4,11 @@ import (
 	"github.com/NFT-com/graph-api/graph/models/api"
 )
 
-func (s *Server) nftTraits(nft *api.NFT, wantRarity bool) ([]*api.Trait, error) {
-
-	// Get trait information for the current NFT.
-	traits, err := s.storage.NFTTraits(nft.ID, wantRarity)
-	if err != nil {
-		s.logError(err).
-			Str("nft", nft.ID).
-			Msg("could not retrieve NFT traits")
-		return nil, errRetrieveTraitsFailed
-	}
-
-	// If we don't need to know the NFT rarity, we're done.
-	if !wantRarity {
-		return traits, nil
-	}
-
-	// Find rarity for missing traits.
-
-	foundTraits := make([]string, 0, len(traits))
-	for _, trait := range traits {
-		foundTraits = append(foundTraits, trait.Type)
-	}
-
-	missingTraits, err := s.storage.NFTMissingTraitRatio(nft.Collection, foundTraits)
-	if err != nil {
-		s.logError(err).
-			Str("nft", nft.ID).
-			Msg("could not retrieve NFT traits")
-		return nil, errRetrieveTraitsFailed
-	}
-
-	traits = append(traits, missingTraits...)
-
-	return traits, nil
-}
+// collectionTraits represents a map of NFT IDs to NFT traits.
+type collectionTraits map[string][]*api.Trait
 
 // FIXME: Create a type instead of using map[string][]*api.Trait
-func (s *Server) getTraitsForCollection(collectionID string) (map[string][]*api.Trait, error) {
+func (s *Server) getTraitsForCollection(collectionID string) (collectionTraits, error) {
 
 	// Get traits for collection.
 	list, err := s.storage.CollectionTraits(collectionID)
