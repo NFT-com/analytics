3104e24ace8304cfc709d55d6e13f66ddad6081c
Maelkum <pathlidge.ann+github@gmail.com>
2022-05-17 05:33:08 +0200

    Reintroduce NFT average price

--- aggregate/api/price.go
+++ aggregate/api/price.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"errors"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -34,5 +33,25 @@ func (a *API) NFTPrice(ctx echo.Context) error {
 
 // NFTAveragePrice handles the request for retrieving the all-time average price of an NFT.
 func (a *API) NFTAveragePrice(ctx echo.Context) error {
-	return errors.New("TBD: Not implemented")
+
+	// Unpack the request.
+	var req apiRequest
+	err := ctx.Bind(&req)
+	if err != nil {
+		return bindError(err)
+	}
+
+	// Retrieve contract address and token ID.
+	nft, err := a.lookupNFT(req.ID)
+	if err != nil {
+		return apiError(err)
+	}
+
+	// Retrieve average price for the NFT.
+	average, err := a.stats.NFTAveragePrice(nft)
+	if err != nil {
+		return apiError(err)
+	}
+
+	return ctx.JSON(http.StatusOK, average)
 }
