3efc32b6132bba6f6505f54fb494493517854421
Maelkum <pathlidge.ann+github@gmail.com>
2022-05-17 08:11:00 +0200

    Move ZeroAddress tothe identifier package

--- aggregate/models/identifier/identifier.go
+++ aggregate/models/identifier/identifier.go
@@ -1,5 +1,9 @@
 package identifier
 
+const (
+	ZeroAddress = "0x0000000000000000000000000000000000000000"
+)
+
 // Address identifier represents a single address on a blockchain.
 type Address struct {
 	ChainID uint
