10699bf4ebf3953c4bafd36a27cce92dd0a1e5cb
Maelkum <pathlidge.ann+github@gmail.com>
2022-05-23 08:09:23 +0200

    Add current price for an NFT

--- aggregate/api/price.go
+++ aggregate/api/price.go
@@ -1,12 +1,25 @@
 package api
 
 import (
-	"errors"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 // NFTPrice handles the request for retrieving historic prices of an NFT.
 func (a *API) NFTPrice(ctx echo.Context) error {
-	return errors.New("TBD: Not implemented")
+
+	// Unpack and validate request.
+	request, err := a.unpackNFTRequest(ctx)
+	if err != nil {
+		return err
+	}
+
+	// Retrieve NFT price.
+	price, err := a.stats.NFTPrice(request.id)
+	if err != nil {
+		return apiError(err)
+	}
+
+	return ctx.JSON(http.StatusOK, price)
 }
