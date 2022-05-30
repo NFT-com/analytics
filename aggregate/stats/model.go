37255e0fe034b8429e404f36f362f598bbf51aa7
Maelkum <pathlidge.ann+github@gmail.com>
2022-05-30 11:37:17 +0200

    Change default data type for metric value

--- aggregate/stats/model.go
+++ aggregate/stats/model.go
@@ -2,24 +2,21 @@ package stats
 
 // batchPriceResult represents the result of the batch NFT price query.
 type batchPriceResult struct {
-	ChainID           uint   `gorm:"column:chain_id"`
-	CollectionAddress string `gorm:"column:collection_address"`
-	TokenID           string `gorm:"column:token_id"`
-	/// FIXME: Change type.
-	TradePrice string `gorm:"column:trade_price"`
+	ChainID           uint    `gorm:"column:chain_id"`
+	CollectionAddress string  `gorm:"column:collection_address"`
+	TokenID           string  `gorm:"column:token_id"`
+	TradePrice        float64 `gorm:"column:trade_price"`
 }
 
 type batchVolumeResult struct {
-	ChainID           uint   `gorm:"column:chain_id"`
-	CollectionAddress string `gorm:"column:collection_address"`
-	// FIXME: Change type
-	Total string `gorm:"column:total"`
+	ChainID           uint    `gorm:"column:chain_id"`
+	CollectionAddress string  `gorm:"column:collection_address"`
+	Total             float64 `gorm:"column:total"`
 }
 
 // FIXME: Could be modeled with the same type as volume.
 type batchMarketCapResult struct {
-	ChainID           uint   `gorm:"column:chain_id"`
-	CollectionAddress string `gorm:"column:collection_address"`
-	// FIXME: Change type
-	Total string `gorm:"column:total"`
+	ChainID           uint    `gorm:"column:chain_id"`
+	CollectionAddress string  `gorm:"column:collection_address"`
+	Total             float64 `gorm:"column:total"`
 }
