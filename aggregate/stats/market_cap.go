ea58c517be479db6da6a3ad38c7a181a5d4c1f82
Maelkum <pathlidge.ann+github@gmail.com>
2022-06-28 22:28:09 +0200

    Switch to a much more performant market cap query (recursive CTE)

--- aggregate/stats/market_cap.go
+++ aggregate/stats/market_cap.go
@@ -10,24 +10,36 @@ import (
 // CollectionMarketCap returns the current market cap for the collection.
 func (s *Stats) CollectionMarketCap(address identifier.Address) (float64, error) {
 
-	// Latest price query will return prices per NFT ranked by freshness.
-	// Prices with the lowest rank (closer to 1) will be the most recent ones.
-	latestPriceQuery := s.db.
-		Table("sales").
-		Select("sales.*, row_number() OVER (PARTITION BY chain_id, collection_address, token_id ORDER BY emitted_at DESC) AS rank").
-		Where("chain_id = ?", address.ChainID).
-		Where("collection_address = ?", address.Address)
-
-	// Summarize query will return the sum of all of the freshest prices for
-	// NFTs in a collection. The query leverages the "latest price" query as a subquery
-	// for the prices.
-	sumQuery := s.db.
-		Table("( ? ) s", latestPriceQuery).
-		Select("SUM(trade_price) AS total").
-		Where("rank = 1")
+	query := s.db.Raw(
+		`WITH RECURSIVE cte AS (
+			(
+				SELECT token_id, trade_price
+				FROM sales
+				WHERE chain_id = ? and collection_address = ?
+				ORDER BY token_id, emitted_at DESC
+				LIMIT 1
+			)
+			UNION ALL
+			SELECT s.*
+			FROM cte c,
+			LATERAL (
+				SELECT token_id, trade_price
+				FROM sales
+				WHERE token_id > c.token_id AND chain_id = ? and collection_address = ?
+				ORDER BY token_id, emitted_at DESC
+				LIMIT 1
+			) s
+		)
+		SELECT SUM(trade_price) AS total
+		FROM cte`,
+		address.ChainID,
+		address.Address,
+		address.ChainID,
+		address.Address,
+	)
 
 	var marketCap datapoint.MarketCap
-	err := sumQuery.Take(&marketCap).Error
+	err := query.Scan(&marketCap).Error
 	if err != nil {
 		return 0, fmt.Errorf("could not retrieve market cap: %w", err)
 	}
