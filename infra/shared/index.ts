aba61141e22f0ee7e9c94c076975e8a3dc993dd3
Joey Punzel <joey@immutableholdings.com>
2022-07-05 15:13:45 -0500

    fix: retry

--- infra/shared/index.ts
+++ infra/shared/index.ts
@@ -3,24 +3,25 @@ import { deployInfra, pulumiOutToValue, getStage } from '../helper'
 import { createRepositories } from './ecr'
 import { createSecurityGroups } from './security-group'
 import * as pulumi from '@pulumi/pulumi';
-import { resolve } from 'path';
 
 const pulumiProgram = async (): Promise<Record<string, any> | void> => {
   const config = new pulumi.Config()
-  //const stackRefName = getResourceName('indexer.shared.us-east-1')
   const stage = getStage()
   const sharedStack = new pulumi.StackReference(`${stage}.indexer.shared.us-east-1`);
   const vpc = sharedStack.getOutput('vpcId') // 'vpc-068564e7eded7ab8b'
   const subnets =  sharedStack.getOutput('subnets') //  ['subnet-0e2f01ec6714dc53f','subnet-0c8aa8a71e35104fc','subnet-08ea44006fecc2ab2']
-  const vpcVal = await pulumiOutToValue(vpc)
+  
+  const vpcVal: pulumi.Output<string> = await pulumiOutToValue(vpc)
+  const subnetVal: pulumi.Output<string> = await pulumiOutToValue(subnets)
+  //const vpc2 = vpc.apply(vpcId => `${vpcId}`)
 
-  const sgs = createSecurityGroups(config, vpcVal) //hardcode test
+  const sgs = createSecurityGroups(config, vpcVal.toString()) //hardcode test
   const { analytics } = createRepositories()
 
   return {
     analyticECRRepo: analytics.name,
-    publicSubnetIds: subnets,
-    vpcId: vpc,
+    publicSubnetIds: subnetVal,
+    vpcId: vpcVal,
     webSGId: sgs.web.id,
   }
 }
