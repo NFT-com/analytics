0b87cbd04dd1dcafb60c0a3ab393da85c341c1a8
Joey Punzel <joey@immutableholdings.com>
2022-07-05 15:42:39 -0500

    fix: retry working setup

--- infra/shared/index.ts
+++ infra/shared/index.ts
@@ -12,14 +12,16 @@ const pulumiProgram = async (): Promise<Record<string, any> | void> => {
   const subnets =  sharedStack.getOutput('subnets') //  ['subnet-0e2f01ec6714dc53f','subnet-0c8aa8a71e35104fc','subnet-08ea44006fecc2ab2']
   const vpcVal: string = await pulumiOutToValue(vpc) //converts output<t> to string
   const subnetVal: string[] = await pulumiOutToValue(subnets)
+  const vpcHard = 'vpc-068564e7eded7ab8b'
+  const subnetsHard = ['subnet-0e2f01ec6714dc53f','subnet-0c8aa8a71e35104fc','subnet-08ea44006fecc2ab2']
 
-  const sgs = createSecurityGroups(config, vpcVal) //hardcode test
+  const sgs = createSecurityGroups(config, vpcHard) //hardcode test
   const { analytics } = createRepositories()
 
   return {
     analyticECRRepo: analytics.name,
-    publicSubnetIds: subnetVal,
-    vpcId: vpcVal,
+    publicSubnetIds: subnetsHard,
+    vpcId: vpcHard,
     webSGId: sgs.web.id,
   }
 }
